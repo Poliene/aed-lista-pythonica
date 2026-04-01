b78253894f766f2bbf23a883fce0266ddc2d1809
Poliene <poliene.f555@email.com>
2026-04-01 11:47:31 -0300

    testnado /

--- src/exercicio_02.py
+++ src/exercicio_02.py
@@ -1,5 +1,5 @@
 def simple_message(message: str) -> str:
-    # recebe uma mensagem
-    # retorna exatamente a mesma mensagem sem alterar
+    # recebe uma mensagem.
+    # retorna exatamente a mesma mensagem sem alterar.
 
     return message
