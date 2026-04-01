a109b4f45e1987a6571d464a2151a913af3d1584
ellie003 <elennaiely8@gmail.com>
2026-04-01 10:16:56 -0300

    exercicio 01 feito e corrigido

--- src/exercicio_01.py
+++ src/exercicio_01.py
@@ -1,8 +1,8 @@
-def hello_world() -> str:
+def hello_world() -> str: # essa função não recebe nada
     """
     Retorna a string padrão de saudação.
 
     Returns:
         str: "Hello, World!"
     """
-    pass
+    return "Hello, World!" # ela só precisa devolver a mensagem padrão, então usa o return
